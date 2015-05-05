acc11a155aa6f9361170a96d2d3c2d14f56a0905
acorbellini <acorbellini@isistan.exa.unicen.edu.ar>
2015-05-05 15:31:35 -0300

    Better networking

--- graphly/scripts/run.sh
+++ graphly/scripts/run.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+
+sudo sysctl -w net.core.rmem_max=26214400
+sudo sysctl -w net.core.wmem_max=26214400 
+
 function getMem {
 	echo $(free -m | tr -s ' ' | head -2 | tail -1 | cut -d' ' -f2)
 }
 
 CP="../../lib/*:./*"
 #-XX:+UseNUMA -XX:+UseParallelGC -XX:+TieredCompilation -XX:+UseCompressedOops 
-OPTIONS="-XX:+UnlockCommercialFeatures -XX:+FlightRecorder -Djava.net.preferIPv4Stack=true -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false"
+#  -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false
+OPTIONS="-Djava.net.preferIPv4Stack=true -XX:+UseCompressedOops -XX:+UnlockCommercialFeatures -XX:+FlightRecorder"
 currMem=$(getMem)
-mem=$(( $currMem - 500))
+mem=$(( $currMem - 1024))
 OPTIONS=$OPTIONS" -Xmx"$mem"m"
 
 if [ $(hostname) == "GridCluster1" ]; then
